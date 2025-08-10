7ab8a6fdd551b062b71d3d7ef6482b13e1f7f4c3
shimul <darkegale3536@gmail.com>
2025-08-10 16:26:23 +0600

    itraction

--- index.js
+++ index.js
@@ -125,3 +125,12 @@ window.addEventListener('scroll',()=>{
     }
   })
 })
+
+//mouse folow interection
+    document.addEventListener('mousemove', function(event) {
+      const motion = document.getElementById("motion");
+      const offsetX = motion.offsetWidth / 2;
+      const offsetY = motion.offsetHeight / 2;
+      motion.style.left = (event.pageX - offsetX) + "px";
+      motion.style.top = (event.pageY - offsetY) + "px";
+    });
